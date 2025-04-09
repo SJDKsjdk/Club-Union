88ec76736b0553e3b05eb787dd6e7a13becf62a1
SJDKsjdk <sjdk@korea.ac.kr>
2025-04-09 13:22:57 +0900

    add show && science
    공연예술분과, 과학기술분과 상세페이지 추가

--- KU_Club_Union_Project/app.py
+++ KU_Club_Union_Project/app.py
@@ -15,6 +15,42 @@ def clubs():
 def soma():
     return render_template('soma.html')
 
+@app.route('/mudan')
+def mudan():
+    return render_template('mudan.html')
+
+@app.route('/kasting')
+def kasting():
+    return render_template('kasting.html')
+
+@app.route('/sum')
+def sum():
+    return render_template('sum.html')
+
+@app.route('/beat')
+def beat():
+    return render_template('beat.html')
+
+@app.route('/rooters')
+def rooters():
+    return render_template('rooters.html')
+
+@app.route('/udf')
+def udf():
+    return render_template('udf.html')
+
+@app.route('/star')
+def star():
+    return render_template('star.html')
+
+@app.route('/icarus')
+def icarus():
+    return render_template('icarus.html')
+
+@app.route('/underdog')
+def underdog():
+    return render_template('underdog.html')
+
 @app.route('/static/<path:filename>')
 def serve_static(filename):
     return send_from_directory('static', filename)
